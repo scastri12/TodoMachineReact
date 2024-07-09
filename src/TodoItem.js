95d1777882c3d431af73a8fe1268e6ead42a688a
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-07-09 10:19:22 -0500

    ci: dinamic styles

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,12 +3,12 @@ import './TodoItem.css';
 function TodoItem({ text, completed }) {
   return (
     <li className="TodoItem">
-      <span className="Icon Icon-check
-      Icon-check--active">
+      <span className={`Icon Icon-check
+      ${completed && "Icon-check--active"}`}>
         V
       </span>
-      <p className="TodoItem-p
-      TodoItem-p--complete">
+      <p className={`TodoItem-p
+      ${completed && "TodoItem-p--complete"}`}>
         {text}
       </p>
       <span className="Icon Icon-delete">
