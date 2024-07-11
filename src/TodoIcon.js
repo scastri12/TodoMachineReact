380f5d4f10ac26e8cbdf480a5513719ff1bc7722
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-07-11 15:06:24 -0500

    ci: css classnames

--- src/TodoIcon.js
+++ src/TodoIcon.js
@@ -3,14 +3,14 @@ import { ReactComponent as CheckSVG } from './check.svg';
 import { ReactComponent as DeleteSVG } from './delete.svg';
 
 const iconTypes = {
-    "complete": <CheckSVG/>,
-    "delete": <DeleteSVG/>
+    "complete": <CheckSVG className="Icon-svg"/>,
+    "delete": <DeleteSVG className="Icon-svg"/>
 };
 
 function TodoIcon({ type }) {
     return (
         <span
-            className={`Icon Icon-svg Icon-${type}`}
+            className={`Icon-container Icon-container-${type}`}
         >
             {iconTypes[type]}
         </span>
