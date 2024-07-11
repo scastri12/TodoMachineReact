ef31156ffff3828d0e3613da88ea65857a87894e
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-07-11 11:44:12 -0500

    ci: add complete and delete items

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 
 const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true},
-  { text: 'Llorar con la llorona', completed: false},
-  { text: 'Regar las plantas', completed: false},
-  { text: 'Estudiar react', completed: false},
-  { text: 'Usar estados derivados', completed: true},
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'Regar las plantas', completed: false },
+  { text: 'Estudiar react', completed: false },
+  { text: 'Usar estados derivados', completed: true },
 ]
 
 function App() {
@@ -18,18 +18,43 @@ function App() {
   const [searchValue, setSearchValue] = React.useState('');
   const completedTodos = todos.filter(todo => todo.completed).length;
   const totalTodos = todos.length;
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLocaleLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(todo => todo.text === text);
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    setTodos(newTodos);
+  }
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(todo => todo.text === text);
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
 
   console.log('los usuarios buscan todos de ' + searchValue);
   return (
     <React.Fragment>
       <TodoCounter completed={completedTodos} total={totalTodos} />
-      <TodoSearch 
+      <TodoSearch
         searchValue={searchValue}
         setSearchValue={setSearchValue}
       />
       <TodoList>
-        {defaultTodos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
         ))}
       </TodoList>
       <CreateTodoButton />
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,17 +1,22 @@
 import './TodoItem.css';
 
-function TodoItem({ text, completed }) {
+function TodoItem({ text, completed, onComplete, onDelete}) {
   return (
     <li className="TodoItem">
       <span className={`Icon Icon-check
-      ${completed && "Icon-check--active"}`}>
+      ${completed && "Icon-check--active"}`}
+      onClick={onComplete}
+      >
         V
       </span>
       <p className={`TodoItem-p
       ${completed && "TodoItem-p--complete"}`}>
         {text}
       </p>
-      <span className="Icon Icon-delete">
+      <span 
+      className="Icon Icon-delete"
+      onClick={onDelete}
+      >
         X
       </span>
     </li>
