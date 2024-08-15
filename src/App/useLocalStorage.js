686a5f7a74c407f779a448c932b5ba12ae0a4642
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-08-15 07:23:30 -0500

    ci: update uselocalstorage

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -1,26 +1,33 @@
+import react from 'react';
 import React from 'react';
 
 function useLocalStorage(itemName, initialValue) {
+  const [item, setItem] = React.useState(initialValue);
+  const [loading, setLoading] = React.useState(initialValue);
 
+
+
+  react.useEffect(() => {
     const localStorageItem = localStorage.getItem(itemName);
     let parsedItem;
     if (!localStorageItem) {
       localStorage.setItem('TODOS_V1', JSON.
-      stringify(initialValue));
+        stringify(initialValue));
       parsedItem = initialValue;
     } else {
       parsedItem = JSON.parse(localStorageItem);
     }
-  
-    const [item, setItem] = React.useState(parsedItem);
-  
-    const saveItem = (newItem) => {
-      localStorage.setItem('TODOS_V1', JSON.
+  });
+
+
+
+  const saveItem = (newItem) => {
+    localStorage.setItem('TODOS_V1', JSON.
       stringify(newItem));
-      setItem(newItem);
-    }; 
-  
-    return [item, saveItem];
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
 }
 
 export { useLocalStorage };
